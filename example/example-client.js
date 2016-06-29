8e9e9dcf039b6350147e05bd88613c9c5363bd33
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-29 19:50:41 +0900

    Reuse sugo-agent-file example file

--- example/example-client.js
+++ example/example-client.js
@@ -1,13 +1,30 @@
-/** This is example of client */
-
 'use strict'
 
-const sugoAgentFile = require('sg-agent-file')
+const sugoAgentFile = require('sugo-agent-file')
 const co = require('co')
 
 co(function * () {
-  let agent = sugoAgentFile('/api/my-docs')
-  yield agent.write('my-data-01.json', JSON.stringify({ foo: 'bar' }, null, 2))
-  let content = yield agent.read('my-data-01.json')
-  /* .. */
+  let agent = sugoAgentFile('http://my-sever.com/files')
+
+  // Check if server available
+  {
+    let ok = yield agent.knock() // Send HTTP HEAD request.
+    /* ... */
+  }
+
+  // Access dynamic files
+  {
+    // Write
+    yield agent.write('my-text-01.txt', 'This is the text.')
+    // Read
+    let content = yield agent.read('my-text-01.txt')
+    console.log(content)
+    /* ... */
+    // Check
+    let exists = yield agent.exists('my-text-01.text')
+    if (exists) {
+      // Delete
+      yield agent.delete('my-text-01.txt')
+    }
+  }
 }).catch((err) => console.error(err))
